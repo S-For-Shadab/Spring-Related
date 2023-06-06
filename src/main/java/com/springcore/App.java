315a9a772b672e8d45316f572237e27b7ad12284
S-For-Shadab <khanshadab8829@gmail.com>
2023-06-06 16:39:30 +0530

    second commit

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -11,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	//please see me i am changing something here
     	ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/config.xml");
     	Student  s1=(Student) context.getBean("student3");
     	System.out.println(s1.toString());
